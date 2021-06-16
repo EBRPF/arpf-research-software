88b22957ac25ce16860b6f782cc4d88a6b871157
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 21:07:34 +0530

    Added organization db entity

---
+++ server/src/main/java/org/rrcat/arpf/entity/Organization.java
@@ -0,0 +1,43 @@
+package org.rrcat.arpf.entity;
+
+import java.util.Objects;
+
+public class Organization {
+    private final String name;
+    private final String type;
+
+    public Organization(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
+}

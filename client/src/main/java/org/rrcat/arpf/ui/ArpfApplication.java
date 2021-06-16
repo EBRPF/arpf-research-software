5b56c5649d228e170e97ae1f13c1a2eab566d057
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-17 00:48:57 +0530

    Added basic fxml loading

--- client/src/main/java/org/rrcat/arpf/ui/ArpfApplication.java
+++ client/src/main/java/org/rrcat/arpf/ui/ArpfApplication.java
@@ -1,19 +1,20 @@
 package org.rrcat.arpf.ui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ArpfApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/layouts/navigation.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
         primaryStage.setTitle("Arpf");
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }

1abe0054db78395e3f82a6e42a6c5c8e8efffb80
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 21:43:14 +0530

    Setup basic window

--- client/src/main/java/org/rrcat/arpf/ui/ArpfApplication.java
+++ client/src/main/java/org/rrcat/arpf/ui/ArpfApplication.java
@@ -1,11 +1,19 @@
 package org.rrcat.arpf.ui;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ArpfApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Arpf");
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }

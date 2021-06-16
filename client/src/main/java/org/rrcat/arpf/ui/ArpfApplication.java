ab86f8d5a8e018605f5e70138be6fd163ba177fe
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-16 21:13:13 +0530

    Added basic application class

---
+++ client/src/main/java/org/rrcat/arpf/ui/ArpfApplication.java
@@ -0,0 +1,11 @@
+package org.rrcat.arpf.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ArpfApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}

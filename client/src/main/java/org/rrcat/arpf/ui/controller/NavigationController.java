57323ac6d7c36c17c8c0eac872043dd6d944f32c
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-06-17 17:04:59 +0530

    Completed header

--- client/src/main/java/org/rrcat/arpf/ui/controller/NavigationController.java
+++ client/src/main/java/org/rrcat/arpf/ui/controller/NavigationController.java
@@ -1,4 +1,32 @@
 package org.rrcat.arpf.ui.controller;
 
-public class NavigationController {
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public final class NavigationController implements Initializable {
+    @FXML
+    private HBox headerBox;
+
+    @FXML
+    private ImageView logoImage;
+
+    public NavigationController() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        logoImage.setFitWidth(70);
+        logoImage.setFitHeight(70);
+    }
 }

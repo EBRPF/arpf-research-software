cbbb0eeaf5407c2777207b960f8423b9f0f7b3c4
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-01 18:37:56 +0530

    Refactored RRCATWebSecurityConfigurerAdapter to reflect new configuration with jwt auth

--- server/src/main/java/org/rrcat/arpf/server/configuration/RRCATWebSecurityConfigurerAdapter.java
+++ server/src/main/java/org/rrcat/arpf/server/configuration/RRCATWebSecurityConfigurerAdapter.java
@@ -1,33 +1,40 @@
 package org.rrcat.arpf.server.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 @Configuration
 @EnableWebSecurity
 public class RRCATWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .realmName("RRCAT Server Applet");
+
+    private final GenericFilterBean jwtFilter;
+
+    public RRCATWebSecurityConfigurerAdapter(@Qualifier("JwtFilter") final GenericFilterBean jwtFilter) {
+        this.jwtFilter = jwtFilter;
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("username")
-                .password(passwordEncoder().encode("password"))
-                .authorities("ROLE_USER");
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/**").authenticated()
+            .antMatchers("/authenticate").permitAll()
+            .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean

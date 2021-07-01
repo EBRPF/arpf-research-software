357eaf929c2ee6ad6c10ba5f20703a8970adec28
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-01 13:41:12 +0530

    Added role permissions

--- server/src/main/java/org/rrcat/arpf/server/auth/Role.java
+++ server/src/main/java/org/rrcat/arpf/server/auth/Role.java
@@ -3,21 +3,23 @@ package org.rrcat.arpf.server.auth;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.rrcat.arpf.server.auth.Permission.*;
+
 public enum  Role {
-    CUSTOMER(),
-    FACILITY_IN_CHARGE(),
-    RP_IN_CHARGE(),
-    ESM_IN_CHARGE(),
-    QC_IN_CHARGE(),
-    OPERATOR();
+    FACILITY_IN_CHARGE(Permission.values()),
+    RP_IN_CHARGE(ORDER_RADIATION_PROCESSING, CHECK_ORDER_STATUS, SAMPLE_IRRADIATION_REPORT),
+    ESM_IN_CHARGE(/* todo: Not yet provided */),
+    QC_IN_CHARGE(ORDER_DOSIMETRY, SHIPPING_DETAILS, SAMPLE_IRRADIATION_REPORT),
+    OPERATOR(ORDER_RADIATION_PROCESSING),
+    ARPF_OFFICE(REGISTER_CUSTOMER, REGISTER_ORDER, SHIPPING_DETAILS);
 
-    private final Collection<String> permissions;
+    private final Collection<Permission> permissions;
 
-    Role(final String... permissions) {
+    Role(final Permission... permissions) {
         this.permissions = Arrays.asList(permissions);
     }
 
-    public Collection<String> getPermissions() {
+    public Collection<Permission> getPermissions() {
         return permissions;
     }
 }

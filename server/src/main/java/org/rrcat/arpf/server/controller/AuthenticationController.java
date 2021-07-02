ea7a7728466d462bc8e1fe6af84ee3602791ac5e
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-07-02 17:14:12 +0530

    Added password validation to AuthenticationController

--- server/src/main/java/org/rrcat/arpf/server/controller/AuthenticationController.java
+++ server/src/main/java/org/rrcat/arpf/server/controller/AuthenticationController.java
@@ -3,27 +3,50 @@ package org.rrcat.arpf.server.controller;
 import org.rrcat.arpf.server.auth.jwt.JwtGenerator;
 import org.rrcat.arpf.server.entity.AuthenticationToken;
 import org.rrcat.arpf.server.entity.LoginRequest;
+import org.rrcat.arpf.server.entity.RrcatUser;
+import org.rrcat.arpf.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/api/v1")
 public final class AuthenticationController {
-    private final JwtGenerator generator;
     private final long authExpirationTime;
+    private final JwtGenerator generator;
+    private final UserRepository repository;
+
 
     @Autowired
-    public AuthenticationController(final JwtGenerator generator, @Value("${auth.expiration_seconds}") final long authExpirationTime) {
-        this.generator = generator;
+    public AuthenticationController(@Value("${auth.expiration_seconds}") final long authExpirationTime, final JwtGenerator generator, UserRepository repository) {
         this.authExpirationTime = authExpirationTime;
+        this.generator = generator;
+        this.repository = repository;
     }
 
     @RequestMapping("/authenticate")
     public @ResponseBody AuthenticationToken authenticate(@RequestBody final LoginRequest loginRequest) {
+        final String uid = loginRequest.getUid();
+        final String requestedPassword = loginRequest.getHashedPassword();
+        final RrcatUser user = repository.findRRCATUserByUid(uid);
+        final String expectedPassword = user.getHashedPassword();
+        if (!Objects.equals(expectedPassword, requestedPassword)) {
+            throw new BadCredentialsException("Invalid password provided.");
+        }
         return generator.generateToken(loginRequest.getUid(), authExpirationTime);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
+    @ResponseStatus(
+            value = HttpStatus.FORBIDDEN,
+            reason = "Failed to authenticate."
+    )
+    public void handleUserNotFound() {
     }
 }

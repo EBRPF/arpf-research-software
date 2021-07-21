3f73dced0c79df3f2a13fef38e90b60da25c10d8
Pratham Wanmode <prathamwanmode3012@gmail.com>
2021-07-22 01:03:20 +0530

    sample_irradiation_report Controller added

---
+++ client/src/main/java/org/rrcat/arpf/ui/controller/SampleIrradReportController.java
@@ -0,0 +1,19 @@
+package org.rrcat.arpf.ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SampleIrradReportController implements Initializable {
+    @FXML
+    private ComboBox<String> OrderNumber;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}

37d89f77a00928732f774c37990ddee92e9e943a
Pratham Wanmode <prathamwanmode3012@gmail.com>
2021-07-26 18:57:09 +0530

    CustomerRegController

---
+++ client/src/main/java/org/rrcat/arpf/ui/api/model/CustomerReg.java
@@ -0,0 +1,70 @@
+package org.rrcat.arpf.ui.api.model;
+
+public class CustomerReg {
+
+    String customerRegistrationNo;
+    String nameOfOrganisation;
+    String researchHeadName;
+    String researchEmailId;
+    Integer researchMobileNo;
+    String officeAddress;
+
+    public String getOfficeAddress() {
+        return officeAddress;
+    }
+
+    public void setOfficeAddress(String officeAddress) {
+        this.officeAddress = officeAddress;
+    }
+
+    public String getResearchEmailId() {
+        return researchEmailId;
+    }
+
+    public void setResearchEmailId(String researchEmailId) {
+        this.researchEmailId = researchEmailId;
+    }
+
+
+
+    public Integer getResearchMobileNo() {
+        return researchMobileNo;
+    }
+
+    public void setResearchMobileNo(Integer researchMobileNo) {
+        this.researchMobileNo = researchMobileNo;
+    }
+
+
+    public String getResearchHeadName() {
+        return researchHeadName;
+    }
+
+    public void setResearchHeadName(String researchHeadName) {
+        this.researchHeadName = researchHeadName;
+    }
+
+    public String getNameOfOrganisation() {
+        return nameOfOrganisation;
+    }
+
+    public void setNameOfOrganisation(String nameOfOrganisation) {
+        this.nameOfOrganisation = nameOfOrganisation;
+    }
+
+
+
+
+
+
+    public CustomerReg( ) {
+    }
+
+    public String getCustomerRegistrationNo() {
+        return customerRegistrationNo;
+    }
+
+    public void setCustomerRegistrationNo(String customerRegistrationNo) {
+        this.customerRegistrationNo = customerRegistrationNo;
+    }
+}
--- client/src/main/java/org/rrcat/arpf/ui/controller/CustomerRegController.java
+++ client/src/main/java/org/rrcat/arpf/ui/controller/CustomerRegController.java
@@ -1,19 +1,27 @@
 package org.rrcat.arpf.ui.controller;
 
 import com.gluonhq.charm.glisten.control.AutoCompleteTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import okio.Options;
+import org.rrcat.arpf.ui.api.model.CustomerReg;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerRegController implements Initializable {
     @FXML
-    private AutoCompleteTextField customerRegNo;
+    private TextField customerRegNo;
     @FXML
     private TextField OrgNameField;
     @FXML
@@ -44,12 +52,79 @@ public class CustomerRegController implements Initializable {
     private TextField AnyOtherInfo;
     @FXML
     private ImageView RegistrationScannedImg;
-
+    @FXML
+    private Button UploadRegScanned;
+    @FXML
+    private Button SaveRecord_Customer;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //Customer Registration Number
+        customerRegNo.setText("EBRPF-Research-");
+        CustomerReg customerReg = new CustomerReg();
+        //Institute Type Combo Box
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "Option 1",
+                        "Option 2",
+                        "Option 3"
+                );
+        InstituteType.setEditable(true);
+        InstituteType.setItems(options);
+        InstituteType.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(InstituteType.getValue());
+            }
+        });
+
+
+        //Save Record
+        SaveRecord_Customer.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(customerRegNo.getText());
+                customerReg.setCustomerRegistrationNo(customerRegNo.getText().trim());
+
+                //Name Of Organisation "fxid : OrgNameField"
+                customerReg.setNameOfOrganisation(OrgNameField.getText().trim());
+
+                //Name Of Organisation "fxid : OrgNameField"
+                customerReg.setNameOfOrganisation(OrgNameField.getText().trim());
+
+
+
+                //Research Activity Head Name "fxid: ResearchHeadName"
+                customerReg.setResearchHeadName(ResearchHeadName.getText().trim());
+
+                //Research Activity Head Mobile Number "fxid: ResearchMobileNo"
+                //if (Integer.parseInt(ResearchMobileNo.getText().trim())==10)
+                  //  customerReg.setResearchMobileNo(Integer.parseInt(ResearchMobileNo.getText().trim()));
+
+                //Research Activity Head Email ID "fxid: ResearchEmail"
+
+                if (validate(ResearchEmail.getText().trim())==true)
+                    customerReg.setResearchEmailId(ResearchEmail.getText().trim());
+                else
+                    System.out.println("Please enter valid Email ID");
+
+                //Office Address "fxid: OfficeAddress"
+                customerReg.setOfficeAddress(OfficeAddress.getText().trim());
+
+                System.out.println(customerRegNo.getText());
+            }
+        });
+
 
     }
 
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
 
 }

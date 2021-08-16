8d4084fbd1c2c443f5f116772e94a1c09c5dc2e6
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-08-16 21:53:46 +0530

    Fixed login

--- client/src/main/java/org/rrcat/arpf/ui/controller/LoginController.java
+++ client/src/main/java/org/rrcat/arpf/ui/controller/LoginController.java
@@ -3,16 +3,12 @@ package org.rrcat.arpf.ui.controller;
 import com.google.common.base.Functions;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import okhttp3.Authenticator;
 import org.dae.arpf.dto.AuthenticationTokenDTO;
@@ -28,37 +24,47 @@ import retrofit2.Response;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
 
 public class LoginController implements Initializable {
     @FXML
-    private Button loginBtn;
+    private TextField username;
     @FXML
-    private TextField usernameTextField;
-    @FXML
-    private PasswordField passwordTextField;
+    private PasswordField password;
 
     private AuthenticationApi api;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
         this.api = RetrofitFactory.create().create(AuthenticationApi.class);
-        usernameTextField.setText("Administrator");
-        passwordTextField.setText("Lo5PofeWw8@r");
+        username.setText("Administrator");
+        password.setText("Lo5PofeWw8@r");
     }
     @FXML
     private void onClickLogin(final ActionEvent event) throws IOException {
         final LoginRequestDTO dto = LoginRequestDTOBuilder.builder()
-                .uid(usernameTextField.getText())
-                .password(passwordTextField.getText())
+                .uid(username.getText())
+                .password(password.getText())
                 .build();
-        final Response<AuthenticationTokenDTO> response = authenticate(dto);
-        final AuthenticationTokenDTO tokenDTO = response.body();
-        if (tokenDTO != null && response.code() == 200) {
-            onAuthenticated(dto);
-        } else {
-            // Failed login
-        }
-
+        final Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Please wait");
+        alert.setContentText("Attempting to login...");
+        alert.showAndWait();
+        username.getParent().setDisable(true);
+        CompletableFuture.supplyAsync(() -> authenticate(dto)).thenAccept((response) -> {
+            alert.notify();
+            alert.close();
+            final AuthenticationTokenDTO tokenDTO = response.body();
+            if (tokenDTO != null && response.code() == 200) {
+                try {
+                    onAuthenticated(dto);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Failed login
+            }
+        });
     }
 
     private Response<AuthenticationTokenDTO> authenticate(final LoginRequestDTO dto) {
@@ -77,7 +83,7 @@ public class LoginController implements Initializable {
         final AuthenticationModule module = new AuthenticationModule(authenticator, loader);
         final Injector injector = Guice.createInjector(module);
         loader.setControllerFactory(injector::getInstance);
-        final Stage stage = (Stage) usernameTextField.getScene().getWindow();
+        final Stage stage = (Stage) username.getScene().getWindow();
         stage.setScene(new Scene(loader.load()));
     }
 }

73d7417e3c7ec252810639ecafadc8d9836cc676
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-08-19 20:27:13 +0530

    Removed unused import

--- server/src/main/java/org/rrcat/arpf/server/repository/CustomerRepository.java
+++ server/src/main/java/org/rrcat/arpf/server/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package org.rrcat.arpf.server.repository;
 
 import org.rrcat.arpf.server.entity.Customer;
-import org.rrcat.arpf.server.entity.UploadedImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 

1c73d0a00ca1009aa0fdc854bfda6de50331bd11
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-08-19 20:25:04 +0530

    Added OrderApi

---
+++ client/src/main/java/org/rrcat/arpf/ui/api/schema/OrderApi.java
@@ -0,0 +1,16 @@
+package org.rrcat.arpf.ui.api.schema;
+
+import org.dae.arpf.dto.OrderDTO;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface OrderApi {
+    @POST("/api/v1/order/register")
+    Call<Void> registerOrder(@Body final OrderDTO dto);
+
+    @GET("/api/v1/order/fetch/{registrationId}")
+    Call<OrderDTO> fetchOrder(@Path("registrationId") final String registrationId);
+}

3dc58cbac4e1d6ed7845d58b3ac385308255a9eb
Vaishnav Anil <vaishnavanil7th@gmail.com>
2022-01-18 20:57:07 +0530

    Removed module data

--- common/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.rrcat.arpf.dto {
-    requires com.fasterxml.jackson.annotation;
-    requires io.soabase.recordbuilder.core;
-    requires com.fasterxml.jackson.databind;
-    requires java.compiler;
-    exports org.dae.arpf.dto;
-}
